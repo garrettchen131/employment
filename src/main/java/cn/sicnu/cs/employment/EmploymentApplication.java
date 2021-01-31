200c3f747e549b59a369e2a459cfeb5bffe7236f
axnihao <974212451@qq.com>
2021-01-31 18:26:49 +0800

    完成项目的初始化工作

---
+++ src/main/java/cn/sicnu/cs/employment/EmploymentApplication.java
@@ -0,0 +1,13 @@
+package cn.sicnu.cs.employment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sicnu/cs/employment/EmploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sicnu.cs.employment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
