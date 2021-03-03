6f3b11d083d543ba11da709af7904bf014c61eb1
Cai8023 <1765116032@qq.com>
2021-03-03 17:51:11 +0800

    添加Kaptcha验证码

--- src/main/java/cn/sicnu/cs/employment/rest/AuthorizeResource.java
+++ src/main/java/cn/sicnu/cs/employment/rest/AuthorizeResource.java
@@ -1,17 +1,26 @@
 package cn.sicnu.cs.employment.rest;
 
+import cn.sicnu.cs.employment.common.Constants;
 import cn.sicnu.cs.employment.common.ResultInfo;
 import cn.sicnu.cs.employment.common.ResultInfoUtil;
 import cn.sicnu.cs.employment.domain.vo.UserVo;
 import cn.sicnu.cs.employment.service.IUserService;
 import cn.sicnu.cs.employment.domain.entity.User;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 
 import static cn.sicnu.cs.employment.common.Constants.*;
 import static cn.sicnu.cs.employment.common.util.RequestUtil.getCurrentUrl;
@@ -22,6 +31,7 @@ import static cn.sicnu.cs.employment.common.util.RequestUtil.getCurrentUrl;
 @RequiredArgsConstructor
 public class AuthorizeResource {
 
+    private final DefaultKaptcha defaultKaptcha;
     private final IUserService userService;
 
     @PostMapping("/register")
@@ -52,5 +62,59 @@ public class AuthorizeResource {
     @GetMapping("/me")
     public ResultInfo<Authentication> me() {
         return ResultInfoUtil.buildSuccess(getCurrentUrl(), SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    /**
+     * 获取验证码图片
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @throws Exception
+     */
+    @GetMapping("/getKap")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws Exception {
+        byte[] captchaChallengeAsJpeg = null;
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            // 生产验证码字符串并保存到session中
+            String createText = defaultKaptcha.createText();
+            httpServletRequest.getSession().setAttribute("kapCode", createText);
+            log.info("生成验证码={}",createText);
+            // 使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        // 定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream = httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+
+    @PostMapping("/checkKap")
+    public ResultInfo<Void> checkKaptchaCode(HttpServletRequest httpServletRequest,
+                                             @RequestParam("code") String getCode) {
+        log.info("接收到的验证码={}",getCode);
+        String kapCode = (String) httpServletRequest.getSession().getAttribute("kapCode");
+        if (kapCode != null){
+            if (kapCode.equals(getCode)){
+                httpServletRequest.getSession().removeAttribute("kapCode");
+                return ResultInfoUtil.buildSuccess(getCurrentUrl());
+            }
+            else {
+                return ResultInfoUtil.buildError(Constants.ERROR_CODE, "验证码错误", getCurrentUrl());
+            }
+        }
+        else {
+            return ResultInfoUtil.buildError(Constants.OTHER_ERROR, "请重新生成验证码", getCurrentUrl());
+        }
     }
 }
